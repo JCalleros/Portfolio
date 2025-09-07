a34240c7ed02e471f03971ee894bcb227d3b5cc6
jcalleros <jcalleros1009@gmail.com>
2025-09-07 10:30:52 -0700

    Added packages and configuration for tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
+import node from "@astrojs/node";
+
+const useNodePreview = !!process.env.ASTRO_PREVIEW;
 
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: useNodePreview ? node({ mode: "standalone" }) : vercel(),
   vite: { plugins: [tailwindcss()] },
 });
