841668e0ea427b87f3f0cdfd63f3a51337ec75d6
jcalleros <jcalleros1009@gmail.com>
2025-09-07 07:10:12 -0700

    Fixed configuration and missing packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// astro.config.mjs
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel/serverless"; // use serverless (Resend SDK isn’t Edge)
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: "server",          // required for /api routes
-  adapter: vercel(),         // deploys as Vercel Functions
-  vite: { plugins: [tailwindcss()] }, // Tailwind v4 (you’re already using @theme + @import "tailwindcss";
+  output: "server",
+  adapter: vercel(),
+  vite: { plugins: [tailwindcss()] },
 });
